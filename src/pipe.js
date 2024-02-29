ee354d9936edbe6ecfe881cfcc2afb5c732bfdc9
Sam Miller <contact.melephas@gmail.com>
2024-02-29 15:22:08 +0000

    Updated .gitignore Fixed indenting and semantic correctness

--- src/pipe.js
+++ src/pipe.js
@@ -1,13 +1,12 @@
 export default function pipe(value, funcs) {
-    if (!Array.isArray(funcs)) {
-        throw new Error("Must pass in array of functions");
-    }
+  if (!Array.isArray(funcs)) {
+    throw new Error("Must pass in array of functions");
+  }
 
-    var currentValue = value;
-    for (const func of funcs) {
-        const newValue = func(currentValue);
-        currentValue = newValue;
-    }
+  let currentValue = value;
+  for (const func of funcs) {
+    currentValue = func(currentValue);
+  }
 
-    return currentValue;
+  return currentValue;
 }
